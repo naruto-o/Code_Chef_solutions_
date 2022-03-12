e0e8eff4a3900a8f7e63d2ffe8f568be3032db10
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-03-12 13:46:00 +0530

    Create add_two_numbers.py

---
+++ add_two_numbers.py
@@ -0,0 +1,7 @@
+# https://www.codechef.com/LP0TO101/submit/FLOW001
+# link for above challenge
+T = int(input())
+for i in range(T):
+  (a,b) = map(int , input().split(' '))
+  ans = a+b
+  print(ans)
