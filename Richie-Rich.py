bea8ed87d0b2142aea33a129580796567b4b9328
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-03-29 00:32:36 +0530

    Create Richie-Rich.py

---
+++ Richie-Rich.py
@@ -0,0 +1,8 @@
+# https://www.codechef.com/LP1TO201/problems/CHFRICH
+# link for above challenge
+t = int(input())
+for i in range(t):
+    a,b,c = map(int,input().split())
+    print((b - a)//c)
+    
+    
